6845a4d3656fc91d7b9de084ec986c985413aa9a
meghaDhiomics <84699361+meghaDhiomics@users.noreply.github.com>
2023-04-20 15:49:46 +0530

    Added CoursesCard,EnrollNow components and Updated AboutUs and Courses folders.

---
+++ src/app/Components/Enroll-Now/EnrollNow.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./EnrollNow.scss";
+
+export default function EnrollNow(){
+    return(
+        <>
+          <div className="footer_section md-12">
+           <div className="footer_container">
+            <div className="footer_content default_text_resources p-5 text-white" >
+           <h1 className="footer_banner_heading">
+                 Build your path to success with AgileiTech.
+           </h1>
+           <h5 className="footer_banner_subHeading mt-3">Register for free demo classes now...!</h5>
+           <button className="enroll_now_button p-1 pr-3 pl-3 mt-3">Enroll Now</button>
+            </div>
+           </div>
+       
+          </div>
+        </>
+    )
+}   
--- src/app/pages/AboutUs/AboutUs.js
+++ src/app/pages/AboutUs/AboutUs.js
@@ -10,13 +10,11 @@ import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import { Add, Remove } from '@mui/icons-material';
 import Person4OutlinedIcon from '@mui/icons-material/Person4Outlined';
 import ShieldOutlinedIcon from '@mui/icons-material/ShieldOutlined';
 import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
 import { BsGlobeCentralSouthAsia } from "react-icons/bs";
+import EnrollNow from "../../Components/Enroll-Now/EnrollNow";
 
 const accordion_data=[
 {
@@ -280,20 +278,11 @@ export default function AboutUs(){
 
 
 {/* __________footer______________ */}
-      <div className="footer_section md-12">
-           <div className="footer_container">
-            <div className="footer_content default_text_resources p-5 text-white" >
-           <h1 className="footer_banner_heading">
-                 Build your path to success with AgileiTech.
-           </h1>
-           <h5 className="footer_banner_subHeading mt-3">Register for free demo classes now...!</h5>
-           <button className="enroll_now_button p-1 pr-3 pl-3 mt-3">Enroll Now</button>
-            </div>
-           </div>
-       
-          </div>
-
-          </div>
+    
+        <div>
+        <EnrollNow/>
+        </div>
+  </div>
         </>
     )
 }
